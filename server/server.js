c4fe0ef4808e8b544121dff2623934f18827a5ef
Om Bhalla <ombhalla29@gmail.com>
2026-06-02 16:21:27 +0530

    temp cors test

--- server/server.js
+++ server/server.js
@@ -19,14 +19,7 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://furniselect-ai.vercel.app"
-    ]
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
